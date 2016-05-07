89138bbfc956fe7934ddd29427c0cb9e43385c30
Andrew Otto <acotto@gmail.com>
2016-05-07 13:06:18 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Usage:
+
+Edit bases.json to list your bases and tables you want to backup.
+
+Make sure you have nodejs and npm installed.
+
+Then run
+
+  ./backup-airtable <apiKey>
+
+Dumps will go to ./Airtable_Backups.
+
---
+++ airtable-dump.js
@@ -0,0 +1,95 @@
+#!/usr/bin/env node
+
+var Airtable = require('airtable');
+var fs = require('fs');
+var cli = require('cli');
+var mkdirp = require('mkdirp');
+var shelljs = require('shelljs');
+
+function build_record(record) {
+    return {
+        "Base Name": record._table._base._name,
+        "Base ID": record._table._base.getId(),
+        "Table Name": record._table.name,
+        "Table ID": record._table.id,
+        "Record ID": record.getId(),
+        "Fields": record.fields
+    };
+}
+
+function dump_table(table, cb) {
+    var dumped_records = [];
+
+    table.select({
+        maxRecords: 100,
+        view: "Main View"
+    }).eachPage(function page(records, fetchNextPage) {
+        records.forEach(function(record) {
+            dumped_records.push(build_record(record));
+        });
+        // To fetch the next page of records, call `fetchNextPage`.
+        // If there are more records, `page` will get called again.
+        // If there are no more records, `done` will get called.
+        fetchNextPage();
+
+    }, function done(error) {
+        if (error) throw error;
+        cb(dumped_records);
+    });
+
+}
+
+function write_backup(file_name, dumped_records) {
+    fs.writeFile(file_name, JSON.stringify(dumped_records, null, 2), function(error) {
+        if (error) throw error;
+        console.log('Wrote ' + file_name);
+    });
+}
+
+
+function dump_base(base, backup_dir) {
+    base.table_names.forEach(function(table_name) {
+        var file_name = backup_dir + '/' + base._name + '.' + table_name + '.json';
+
+        dump_table(
+            base(table_name),
+            write_backup.bind(undefined, file_name.replace(/\s+/g, "_"))
+        );
+    });
+}
+
+
+function backup(bases, backup_dir) {
+    mkdirp.sync(backup_dir);
+
+    for (var base_name in bases) {
+        var base = Airtable.base(bases[base_name].id);
+
+        // Save base name and table names from bases.json
+        // so we don't have to pass them around.
+        base._name = base_name;
+        base.table_names = bases[base_name].tables;
+
+        dump_base(base, backup_dir);
+    }
+    // write time of backup
+    fs.writeFileSync(backup_dir + '/timestamp.txt', new Date().toISOString());
+}
+
+function zip_directory(dir) {
+
+}
+
+
+//  Read list of bases out of bases.json
+var bases = require('./bases.json');
+
+// Read API key from CLI
+var api_key = process.argv[2];
+Airtable.configure({apiKey: api_key});
+
+var backup_ts =  new Date().toISOString();
+var backup_dir = 'Airtable_Backups/' + backup_ts
+console.log("Backing up the following bases into " + backup_dir + ":\n ", Object.keys(bases).join('\n  '));
+
+backup(bases, backup_dir);
